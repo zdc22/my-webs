8e3ab13f1846322d00a2082cbe6f49b7d471eb7e
zeta <bbistylez@gmail.com>
2022-06-12 21:44:12 -0300

    New Portfolio Web

---
+++ Portfolio/main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal(".hero");
+ScrollReveal().reveal(".about", { delay: 300 });
+ScrollReveal().reveal(".services", { delay: 300 });
+ScrollReveal().reveal(".hire", { delay: 300 });
+ScrollReveal().reveal(".footer", { delay: 300 });
